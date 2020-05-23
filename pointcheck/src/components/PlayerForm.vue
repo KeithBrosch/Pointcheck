a9369ab5280667190ed4fe2cf8b0a7bff07ebd9b
Keith Brosch <Keith.Brosch@wayne.edu>
2020-05-22 20:15:06 -0400

    layout

---
+++ pointcheck/src/components/PlayerForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="player-form">
+    <label>Gamertag 1</label>
+    <br>
+    <input type="text" v-model="gamertag1"/>
+    <br>
+    <br>
+    <label>Gamertag 2</label>
+    <br>
+    <input type="text" v-model="gamertag2"/>
+    <br>
+    <div v-if="!(gamertag1.length && gamertag2.length)" class="info-container">
+    <p class="info">* you must enter the CURRENT gamertag on the account</p>
+    <p class="info">* only matches from before 2011 are recoverable</p>
+    </div>
+    <button v-if="gamertag1.length && gamertag2.length" @click="submit()"
+    type="submit">Search</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'PlayerForm',
+  data() {
+    return {
+      gamertag1: '',
+      gamertag2: '',
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.player-form {
+    width: 500px;
+    height: 300px;
+    padding: 50px;
+    color: #fff;
+    text-align: center;
+}
+input {
+    width: 200px;
+    height: 30px;
+    padding: 5px 5px 5px 10px;
+    margin: 10px 0;
+}
+.info-container {
+    height: 80px;
+}
+.info {
+    margin-top: 20px;
+    font-size: .8em;
+}
+button {
+    height: 40px;
+    margin-top: 20px;
+    width: 80px;
+    background: transparent;
+    border: 1px solid #fff;
+    outline: none;
+    color: #fff;
+    cursor: pointer;
+}
+button:hover {
+    background: rgb(22, 108, 191);
+    border-color: rgb(22, 108, 191);
+}
+button:active {
+    border-color: rgb(22, 108, 191)
+}
+</style>
--- pointcheck/src/views/Home.vue
+++ pointcheck/src/views/Home.vue
@@ -1,18 +1,95 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <div class="nav-content">
+        <div class="headline">
+          <h1>Pointcheck</h1>
+        </div>
+      <div class="details">Front End Repo</div>
+      <div class="footer">created by <a href="https://twitter.com/keithbrosch" target="blank">@keithbrosch</a> and <a href="https://twitter.com/kifflom" target="blank">@kifflom</a>
+      <br><br> <a href="#"><center>donate</center></a></div>
+      </div>
+    </nav>
+    <div class="content">
+      <PlayerForm/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import PlayerForm from '@/components/PlayerForm.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    PlayerForm,
   },
 };
 </script>
+
+<style lang="css" scoped>
+.home {
+  /* background-image: linear-gradient(to right bottom, #d16ba5, #c777b9, #ba83ca,
+  #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1); */
+  background: linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(22, 108, 191, 0.7)),
+  url('../assets/images/Narrows01.jpg');
+  background-position: center center;
+  background-size: cover;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  /* overflow: hidden; */
+}
+nav {
+  width: 400px;
+  background: #fff;
+  height: 100%;
+  box-shadow: 2px 0 5px -2px #000;
+  transition: margin .5s ease-in-out;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: -350px;
+}
+nav:hover {
+  margin-left: 0;
+  transition: margin .5s ease-in-out;
+}
+.nav-content {
+  height: 90%;
+  padding: 0 20px;
+}
+h1, h2, h3, p, span, ul {
+  height: auto;
+   padding: 0 20px;
+}
+.headline {
+  height: 20%;
+}
+.headline h1 {
+  font-size: 2em;
+  font-weight: 700;
+}
+.details   {
+  height: 70%;
+  padding: 0 20px;
+}
+.name {
+  font-weight: 700;
+}
+.footer {
+  height: 10%;
+  font-size: 14px;
+}
+.footer a {
+  text-decoration: none;
+}
+.content {
+  flex: auto;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
