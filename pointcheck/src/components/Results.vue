debef16c8c477c7e0590ce2e840bd8ca83debfaf
Keith Brosch <Keith.Brosch@wayne.edu>
2020-05-23 14:36:14 -0400

    results layout

--- pointcheck/src/components/Results.vue
+++ pointcheck/src/components/Results.vue
@@ -1,10 +1,31 @@
 <template>
-    <div class="results">
-        <div id="player1">
-            player1
-        </div>
-        <div id="player2">
-            player2
+    <div id="results">
+        <div class="inner">
+            <div class="tags">
+                <div id="player1">
+                    <h1>{{results.player1.name}}</h1>
+                    <div class="stats">
+                        stats
+                    </div>
+                </div>
+                <div id="player2">
+                    <h1>{{results.player2.name}}</h1>
+                    <div class="stats">
+                        stats
+                    </div>
+                </div>
+            </div>
+            <div class="matches">
+                <div v-for="(match, index) in results.matches" :key="index" class="match"
+                :class="{'heretic' : match.map === 'Heretic',
+                'foundry' : match.map === 'Foundry',
+                'guardian' : match.map === 'Guardian',
+                'narrows' : match.map === 'Narrows',
+                'pit' : match.map === 'Pit',
+                'construct' : match.map === 'Construct'}">
+                    match
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,44 +38,103 @@ export default {
   },
   mounted() {
     this.$emit('expandNav');
-    const player1Div = document.getElementById('player1');
-    player1Div.style.marginTop = 0;
-    const player2Div = document.getElementById('player2');
-    player2Div.style.marginTop = 0;
+    const results = document.getElementById('results');
+    results.style.opacity = '1';
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.results {
+#results {
     width: 100%;
     height: 100%;
+    color: #fff;
+    background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5));
+    opacity: 0;
+    transition: opacity 1s ease-in-out;
     display: flex;
     align-items: center;
     justify-content: center;
-    >#player1 {
-        text-align: center;
-        background: blue;
-        flex: 1;
-        height: 100%;
-        margin-top: 200vh;
-        transition: margin .4s ease-in-out;
-        transition-delay: .8s;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    >#player2 {
-        background: green;
-        text-align: center;
-        flex: 1;
-        height: 100%;
-        margin-top: -200vh;
-        transition: margin .4s ease-in-out;
-        transition-delay: 1.2s;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+    >.inner {
+        height: 90%;
+        width: 100%;
+        >.tags {
+            display: flex;
+            align-items: flex-start;
+            justify-content: center;
+            margin-bottom: 50px;
+            >#player1 {
+                text-align: center;
+                flex: 1;
+                height: 100%;
+                >.stats {
+                    background: transparent;
+                    border: 1px solid #fff;
+                    width: 50%;
+                    margin: 0 auto;
+                    padding: 20px 10px;
+                }
+            }
+            >#player2 {
+                text-align: center;
+                flex: 1;
+                height: 100%;
+                >.stats {
+                    background: transparent;
+                    border: 1px solid #fff;
+                    width: 50%;
+                    margin: 0 auto;
+                    padding: 20px 10px;
+                }
+            }
+        }
+        >.matches {
+            width: 90%;
+            margin: 0 auto;
+            >.match {
+                border: 1px solid #fff;
+                margin-bottom: 10px;
+                height: 100px;
+                background-size: cover;
+                background-repeat: no-repeat;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                transition: transform 0.3s ease-in-out;
+                &:hover {
+                        transform: scale(1.05);
+                    }
+                &.heretic {
+                    background: url('../assets/images/heretic.jpg');
+                    background-position: 50% 100%;
+                }
+                &.narrows {
+                    background: url('../assets/images/narrows.jpg');
+                    background-position: bottom;
+                    background-position: 10% 65%;
+                }
+                &.pit {
+                    background: url('../assets/images/pit.jpg');
+                    background-position: bottom;
+                    background-position: 50% 50%;
+                }
+                &.guardian {
+                    background: url('../assets/images/guardian.jpg');
+                    background-position: bottom;
+                    background-position: 50% 0%;
+                }
+                &.construct {
+                    background: url('../assets/images/construct.jpg');
+                    background-position: bottom;
+                    background-position: 80% 50%;
+                }
+                &.foundry {
+                    background: url('../assets/images/foundry.jpg');
+                    background-position: bottom;
+                    background-position: 50% 50%;
+                }
+            }
+        }
     }
 }
 </style>
